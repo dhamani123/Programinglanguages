8b36f071ea09191781a32d8cd79a6a4cca90ccd4
Dhamani Devi.L <lekkaladhamani@gmail.com>
2019-01-31 12:41:47 +0530

    sum

---
+++ c/sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int Number1, Number2,sum = 0;
+printf("Enter first and second number");
+scanf("%d,%d",&Number1,&Number2);
+sum = Number1 + Number2;
+printf(" The sum of %d and %d is %d.", Number1, Number2, sum);
+}
